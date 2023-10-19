bf0a23495b7034276a822b6be29a4f5a0d521b19
Lejito <alejocrrs@gmail.com>
2023-10-19 00:14:30 -0500

    Fix: Remove unnecessary param idCuenta

--- src/controllers/cuentas.controller.js
+++ src/controllers/cuentas.controller.js
@@ -16,15 +16,15 @@ class CuentasController {
           replacements: { idUsuario }
         }
       );
-  
+
       const idCuenta = consultarCodigoCuenta[0][0].consultar_codigo_cuenta_id_usuario;
-  
+
       return idCuenta;
     } catch (error) {
       return -1;
     }
   }
-  
+
   async consultarIdCuentaNumeroTelefonoAUX(numeroTelefono) {
     try {
       const consultarCodigoCuentaNumeroTelefono = await sequelize.query(
@@ -33,9 +33,9 @@ class CuentasController {
           replacements: { numeroTelefono }
         }
       );
-  
+
       const idCuenta = consultarCodigoCuentaNumeroTelefono[0][0].consultar_codigo_cuenta_numero_telefono;
-  
+
       return idCuenta;
     } catch (error) {
       console.log(error);
@@ -82,36 +82,36 @@ class CuentasController {
   */
   async consultarDatos(req, res) {
     try {
-      const requiredParams = [PARAM_ID_CUENTA];
+      const token = req.headers.authorization;
+      const { idUsuario } = jwt.verify(token, process.env.SECRETJWT);
 
-      if (utils.validateBody(req, res, requiredParams)) {
-        const { idCuenta } = req.body;
+      const idCuenta = await CuentasController.prototype.consultarIdCuentaIdUsuarioAUX(idUsuario);
 
-        const consultarCuenta = await sequelize.query(
-          "SELECT * FROM consultar_cuenta(:idCuenta::INT);",
-          {
-            replacements: { idCuenta }
-          }
-        );
-
-        if (consultarCuenta[0].length > 0) {
-          const id = consultarCuenta[0][0].id;
-          const numeroTelefono = consultarCuenta[0][0].numero_telefono;
-          const idUsuario = consultarCuenta[0][0].id_usuario;
-          const habilitada = consultarCuenta[0][0].habilitada;
-          const saldoOculto = consultarCuenta[0][0].saldo_oculto;
-
-          res.status(200).json(utils.successResponse(
-            "Datos de la cuenta recuperados correctamente.",
-            { cuenta: { id, numeroTelefono, idUsuario, habilitada, saldoOculto } }
-          ));
-        } else {
-          res.status(200).json(utils.errorResponse(
-            "No se encontró ninguna cuenta con el id especificado.",
-            null
-          ));
+      const consultarCuenta = await sequelize.query(
+        "SELECT * FROM consultar_cuenta(:idCuenta::INT);",
+        {
+          replacements: { idCuenta }
         }
+      );
+
+      if (consultarCuenta[0].length > 0) {
+        const id = consultarCuenta[0][0].id;
+        const numeroTelefono = consultarCuenta[0][0].numero_telefono;
+        const idUsuario = consultarCuenta[0][0].id_usuario;
+        const habilitada = consultarCuenta[0][0].habilitada;
+        const saldoOculto = consultarCuenta[0][0].saldo_oculto;
+
+        res.status(200).json(utils.successResponse(
+          "Datos de la cuenta recuperados correctamente.",
+          { cuenta: { id, numeroTelefono, idUsuario, habilitada, saldoOculto } }
+        ));
+      } else {
+        res.status(200).json(utils.errorResponse(
+          "No se encontró ninguna cuenta con el id especificado.",
+          null
+        ));
       }
+
     } catch (error) {
       res.status(500).json(utils.errorResponse(
         "Ha ocurrido un error en el servidor.",
@@ -127,33 +127,33 @@ class CuentasController {
   */
   async consultarSaldo(req, res) {
     try {
-      const requiredParams = [PARAM_ID_CUENTA];
+      const token = req.headers.authorization;
+      const { idUsuario } = jwt.verify(token, process.env.SECRETJWT);
 
-      if (utils.validateBody(req, res, requiredParams)) {
-        const { idCuenta } = req.body;
+      const idCuenta = await CuentasController.prototype.consultarIdCuentaIdUsuarioAUX(idUsuario);
 
-        const consultarSaldo = await sequelize.query(
-          "SELECT * FROM consultar_saldo(:idCuenta::INT);",
-          {
-            replacements: { idCuenta }
-          }
-        );
-
-        if (consultarSaldo[0].length > 0) {
-          const saldo = parseFloat(consultarSaldo[0][0].saldo);
-          const saldoBolsillos = parseFloat(consultarSaldo[0][0].saldo_bolsillos);
-
-          res.status(200).json(utils.successResponse(
-            "Saldo de la cuenta recuperados correctamente.",
-            { cuenta: { saldo, saldoBolsillos } }
-          ));
-        } else {
-          res.status(200).json(utils.errorResponse(
-            "No se encontró ninguna cuenta con el id especificado.",
-            null
-          ));
+      const consultarSaldo = await sequelize.query(
+        "SELECT * FROM consultar_saldo(:idCuenta::INT);",
+        {
+          replacements: { idCuenta }
         }
+      );
+
+      if (consultarSaldo[0].length > 0) {
+        const saldo = parseFloat(consultarSaldo[0][0].saldo);
+        const saldoBolsillos = parseFloat(consultarSaldo[0][0].saldo_bolsillos);
+
+        res.status(200).json(utils.successResponse(
+          "Saldo de la cuenta recuperados correctamente.",
+          { cuenta: { saldo, saldoBolsillos } }
+        ));
+      } else {
+        res.status(200).json(utils.errorResponse(
+          "No se encontró ninguna cuenta con el id especificado.",
+          null
+        ));
       }
+
     } catch (error) {
       res.status(500).json(utils.errorResponse(
         "Ha ocurrido un error en el servidor.",
@@ -169,32 +169,30 @@ class CuentasController {
   */
   async activarSaldoOculto(req, res) {
     try {
-      const requiredParams = [PARAM_ID_CUENTA];
+      const token = req.headers.authorization;
+      const { idUsuario } = jwt.verify(token, process.env.SECRETJWT);
 
-      if (utils.validateBody(req, res, requiredParams)) {
-        const token = req.headers.authorization;
-        const { idUsuario } = jwt.verify(token, process.env.SECRETJWT);
-        const { idCuenta } = req.body;
+      const idCuenta = await CuentasController.prototype.consultarIdCuentaIdUsuarioAUX(idUsuario);
 
-        const activarSaldoOculto = await sequelize.query(
-          "SELECT activar_saldo_oculto(:idUsuario::INT, :idCuenta::INT);",
-          {
-            replacements: { idUsuario, idCuenta }
-          }
-        );
-
-        if (activarSaldoOculto[0][0].activar_saldo_oculto) {
-          res.status(200).json(utils.successResponse(
-            "Saldo oculto activado correctamente.",
-            {}
-          ));
-        } else {
-          res.status(200).json(utils.errorResponse(
-            "No se encontró ninguna cuenta con el id especificado.",
-            null
-          ));
+      const activarSaldoOculto = await sequelize.query(
+        "SELECT activar_saldo_oculto(:idUsuario::INT, :idCuenta::INT);",
+        {
+          replacements: { idUsuario, idCuenta }
         }
+      );
+
+      if (activarSaldoOculto[0][0].activar_saldo_oculto) {
+        res.status(200).json(utils.successResponse(
+          "Saldo oculto activado correctamente.",
+          {}
+        ));
+      } else {
+        res.status(200).json(utils.errorResponse(
+          "No se encontró ninguna cuenta con el id especificado.",
+          null
+        ));
       }
+
     } catch (error) {
       res.status(500).json(utils.errorResponse(
         "Ha ocurrido un error en el servidor.",
@@ -210,32 +208,30 @@ class CuentasController {
   */
   async desactivarSaldoOculto(req, res) {
     try {
-      const requiredParams = [PARAM_ID_CUENTA];
+      const token = req.headers.authorization;
+      const { idUsuario } = jwt.verify(token, process.env.SECRETJWT);
 
-      if (utils.validateBody(req, res, requiredParams)) {
-        const token = req.headers.authorization;
-        const { idUsuario } = jwt.verify(token, process.env.SECRETJWT);
-        const { idCuenta } = req.body;
+      const idCuenta = await CuentasController.prototype.consultarIdCuentaIdUsuarioAUX(idUsuario);
 
-        const desactivarSaldoOculto = await sequelize.query(
-          "SELECT desactivar_saldo_oculto(:idUsuario::INT, :idCuenta::INT);",
-          {
-            replacements: { idUsuario, idCuenta }
-          }
-        );
-
-        if (desactivarSaldoOculto[0][0].desactivar_saldo_oculto) {
-          res.status(200).json(utils.successResponse(
-            "Saldo oculto desactivado correctamente.",
-            {}
-          ));
-        } else {
-          res.status(200).json(utils.errorResponse(
-            "No se encontró ninguna cuenta con el id especificado.",
-            null
-          ));
+      const desactivarSaldoOculto = await sequelize.query(
+        "SELECT desactivar_saldo_oculto(:idUsuario::INT, :idCuenta::INT);",
+        {
+          replacements: { idUsuario, idCuenta }
         }
+      );
+
+      if (desactivarSaldoOculto[0][0].desactivar_saldo_oculto) {
+        res.status(200).json(utils.successResponse(
+          "Saldo oculto desactivado correctamente.",
+          {}
+        ));
+      } else {
+        res.status(200).json(utils.errorResponse(
+          "No se encontró ninguna cuenta con el id especificado.",
+          null
+        ));
       }
+
     } catch (error) {
       res.status(500).json(utils.errorResponse(
         "Ha ocurrido un error en el servidor.",
@@ -287,41 +283,35 @@ class CuentasController {
     try {
       const token = req.headers.authorization;
 
-      if (token) {
-        const { idUsuario } = jwt.verify(token, process.env.SECRETJWT);
-        const idCuenta = await CuentasController.prototype.consultarIdCuentaIdUsuarioAUX(idUsuario);
-        const requiredParams = [PARAM_MONTO];
+      const { idUsuario } = jwt.verify(token, process.env.SECRETJWT);
+      const idCuenta = await CuentasController.prototype.consultarIdCuentaIdUsuarioAUX(idUsuario);
+      const requiredParams = [PARAM_MONTO];
 
-        if (utils.validateBody(req, res, requiredParams)) {
-          const { monto } = req.body;
+      if (utils.validateBody(req, res, requiredParams)) {
+        const { monto } = req.body;
 
-          const verificarSaldoSuficiente = await sequelize.query(
-            "SELECT verificar_saldo_suficiente(:idCuenta::INT, :monto::DECIMAL(16,2));",
-            {
-              replacements: { idCuenta, monto }
-            }
-          );
-
-          const saldoSuficiente = verificarSaldoSuficiente[0][0].verificar_saldo_suficiente;
-
-          if (saldoSuficiente != null) {
-            res.status(200).json(utils.successResponse(
-              "Verificación de saldo realizada correctamente.",
-              { saldoSuficiente }
-            ));
-          } else {
-            res.status(200).json(utils.warningResponse(
-              "No se encontró ninguna cuenta.",
-              null
-            ));
+        const verificarSaldoSuficiente = await sequelize.query(
+          "SELECT verificar_saldo_suficiente(:idCuenta::INT, :monto::DECIMAL(16,2));",
+          {
+            replacements: { idCuenta, monto }
           }
+        );
+
+        const saldoSuficiente = verificarSaldoSuficiente[0][0].verificar_saldo_suficiente;
+
+        if (saldoSuficiente != null) {
+          res.status(200).json(utils.successResponse(
+            "Verificación de saldo realizada correctamente.",
+            { saldoSuficiente }
+          ));
+        } else {
+          res.status(200).json(utils.warningResponse(
+            "No se encontró ninguna cuenta.",
+            null
+          ));
         }
-      } else {
-        res.status(200).json(utils.errorResponse(
-          'No se puede verificar la suficiencia de saldo. Autenticación no proporcionada.',
-          null
-        ));
       }
+
     } catch (error) {
       res.status(500).json(utils.errorResponse(
         "Ha ocurrido un error en el servidor.",
